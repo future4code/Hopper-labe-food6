b4d75e8e71cfefe7ddf5b974a388e0fe002d4147
MadsonPereira <madson.ribas@gmail.com>
2022-07-29 22:05:08 -0300

    madson2

--- four-food/src/App.jsx
+++ four-food/src/App.jsx
@@ -2,8 +2,8 @@ import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import {theme} from "./Constantes/theme"
 import './App.css';
-import Rotas from './Madson/Components/Rotas';
-import { GlobalState } from './Madson/Global/GlobalState';
+import Rotas from './Components/Rotas';
+import { GlobalState } from './Global/GlobalState';
 
 
 function App() {
